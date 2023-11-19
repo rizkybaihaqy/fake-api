f3a3b91198659b003719f066a5fae2181c7bda97
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2023-11-19 16:35:24 +0700

    Add relationship

--- app/routes/notes.js
+++ app/routes/notes.js
@@ -1,8 +1,16 @@
-import {DB} from '#utils/db.js'
-import {NumberFromString} from '#utils/codec.js'
-import express from 'express'
-import {Codec, Right, optional, string} from 'purify-ts'
 import {Note} from '#model/note.js'
+import {User} from '#model/user.js'
+import {NumberFromString} from '#utils/codec.js'
+import {DB} from '#utils/db.js'
+import express from 'express'
+import {
+  Codec,
+  Either,
+  Right,
+  Tuple,
+  optional,
+  string
+} from 'purify-ts'
 
 const notes = express.Router()
 
@@ -19,6 +27,48 @@ notes.get('/notes', (req, res) => {
       status: 400
     }))
     .ifRight((notes) => res.json(notes))
+    .ifLeft((error) => res.status(400).json(error))
+})
+
+notes.get('/users/:id/notes', (req, res) => {
+  Codec.interface({
+    _limit: optional(NumberFromString),
+    _page: optional(NumberFromString),
+    note: optional(string)
+  })
+    .decode(req.query)
+    .map((query) => ({...query, userId: req.params.id}))
+    .chain(DB(res.locals.seed, Note).fetch)
+    .chain((notes) =>
+      DB(res.locals.seed, User)
+        .get(req.params.id)
+        .map((user) => ({user, ...notes}))
+    )
+    .mapLeft((error) => ({
+      message: error,
+      status: 400
+    }))
+    .ifRight((notes) => res.json(notes))
+    .ifLeft((error) => res.status(400).json(error))
+})
+
+notes.get('/users/:userId/notes/:noteId', (req, res) => {
+  Right({userId: req.params.userId, noteId: req.params.noteId})
+    .chain(({noteId, userId}) =>
+      DB(res.locals.seed, Note)
+        .get(noteId)
+        .map((note) => ({note, userId}))
+    )
+    .chain(({note, userId}) =>
+      DB(res.locals.seed, User)
+        .get(userId)
+        .map((user) => ({note, user}))
+    )
+    .mapLeft((error) => ({
+      message: error,
+      status: 400
+    }))
+    .ifRight((data) => res.json(data))
     .ifLeft((error) => res.status(400).json(error))
 })
 
